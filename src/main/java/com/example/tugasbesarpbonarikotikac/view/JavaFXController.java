914320d628fb5819105f90449f89bd3f191e522a
Gambitz96 <devissuherma8278@gmail.com>
2026-07-03 17:28:38 +0700

    feat: Menghubungkan kolom tabel dengan data putusan

--- src/main/java/com/example/tugasbesarpbonarikotikac/view/JavaFXController.java
+++ src/main/java/com/example/tugasbesarpbonarikotikac/view/JavaFXController.java
@@ -6,6 +6,7 @@ import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -51,6 +52,24 @@ public class Controller implements Initializable {
         cbFilterJenis.getItems().addAll("Semua Jenis", "Sabu-sabu", "Ganja", "Ekstasi", "Heroin", "Kokain");
         cbFilterJenis.getSelectionModel().select(0);
         return 0;
+    }
+    public void tampilkanDaftarPutusan() {
+        colNomor.setCellValueFactory(new PropertyValueFactory<>("nomorPerkara"));
+        colPengadilan.setCellValueFactory(new PropertyValueFactory<>("pengadilan"));
+        colTanggal.setCellValueFactory(new PropertyValueFactory<>("tanggalPutusan"));
+        colNama.setCellValueFactory(new PropertyValueFactory<>("namaTerdakwa"));
+        colUmur.setCellValueFactory(new PropertyValueFactory<>("umurTerdakwa"));
+        colJenis.setCellValueFactory(new PropertyValueFactory<>("jenisNarkotika"));
+        colBerat.setCellValueFactory(new PropertyValueFactory<>("beratBarangBukti"));
+        colPasal.setCellValueFactory(new PropertyValueFactory<>("pasalDilanggar"));
+        colPeran.setCellValueFactory(new PropertyValueFactory<>("peranTerdakwa"));
+        colVonisBulan.setCellValueFactory(new PropertyValueFactory<>("vonisHukumanBulan"));
+        colVonisDenda.setCellValueFactory(new PropertyValueFactory<>("vonisDendaRupiah"));
+        colHakim.setCellValueFactory(new PropertyValueFactory<>("namaHakim"));
+
+        filteredData = new FilteredList<>(dataPutusan, p -> true);
+        putusanTable.setItems(filteredData);
+        tampilkanStatistik();
     }
 }
 
