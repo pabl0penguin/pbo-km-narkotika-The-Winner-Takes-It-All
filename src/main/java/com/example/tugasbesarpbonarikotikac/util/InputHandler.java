409c067935831c3e396934680b605d8ed0584bbd
pabl0penguin <rizqi.erlangga0106@gmail.com>
2026-07-03 17:19:23 +0700

    feat: menambahkan validasi dan private constructor pada InputHandler

--- src/main/java/com/example/tugasbesarpbonarikotikac/util/InputHandler.java
+++ src/main/java/com/example/tugasbesarpbonarikotikac/util/InputHandler.java
@@ -2,6 +2,16 @@ package com.example.tugasbesarpbonarikotikac.util;
 import java.util.Scanner;
 
 public class InputHandler {
+    private static final int UMUR_MIN = 10;
+    private static final int UMUR_MAX = 100;
+    private static final double BERAT_MIN = 0.001;   // gram
+    private static final int VONIS_MIN_BULAN = 1;
+    private static final int VONIS_MAX_BULAN = 600;     // 50 tahun
+    private static final double DENDA_MIN = 0.0;
+    private static final int MAX_RETRY = 5;       // batas percobaan input
 
+    private InputHandler() {
+
+    }
 
 }
