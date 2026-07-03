c974c9aa4e854c21df703ed199c3f4cdb9905fb4
unknown <raihansinatriaoktav@gmail.com>
2026-07-03 19:05:12 +0700

    feat: tambah atribut data putusan dan static counter

--- src/main/java/com/example/tugasbesarpbonarikotikac/model/Putusan.java
+++ src/main/java/com/example/tugasbesarpbonarikotikac/model/Putusan.java
@@ -1,4 +1,18 @@
 package com.example.tugasbesarpbonarikotikac.model;
 
 public class Putusan {
+    private String nomorPerkara;
+    private String pengadilan;
+    private String tanggalPutusan;
+    private String namaTerdakwa;
+    private int umurTerdakwa;
+    private String jenisNarkotika;
+    private double beratBarangBukti;
+    private String pasalDilanggar;
+    private String peranTerdakwa;
+    private int vonisHukuman;
+    private double vonisDenda;
+    private String namaHakim;
+
+    private static int jumlahDibuat = 0;
 }
