008d8b72075306f10bf2e4a7c26c1ada56e27ae0
Gambitz96 <devissuherma8278@gmail.com>
2026-07-03 17:19:14 +0700

    feat: Tambah package com.example.tugasbesarpbonarikotikac.view, Deklarasikan TableView beserta semua TableColumn, Deklarasikan seluruh TextField untuk form input, Deklarasi ComboBox untuk search & filter, Tambah Label untuk statistik dan variabel data

--- src/main/java/com/example/tugasbesarpbonarikotikac/view/JavaFXController.java
+++ src/main/java/com/example/tugasbesarpbonarikotikac/view/JavaFXController.java
@@ -1,14 +1,28 @@
 package com.example.tugasbesarpbonarikotikac.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class JavaFXController {
-    @FXML
-    private Label welcomeText;
+public class Controller implements Initializable {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-}
+
+    @FXML private TableView<Putusan> putusanTable;
+    @FXML private TableColumn<Putusan, String> colNomor, colPengadilan, colTanggal, colNama, colJenis, colPasal, colPeran, colHakim;
+    @FXML private TableColumn<Putusan, Integer> colUmur, colVonisBulan;
+    @FXML private TableColumn<Putusan, Double> colBerat, colVonisDenda;
+
+    @FXML private TextField tfNomorPerkara, tfPengadilan, tfTanggal, tfNamaTerdakwa, tfUmur;
+    @FXML private TextField tfJenisNarkotika, tfBerat, tfPasal, tfPeran, tfVonisBulan, tfVonisDenda, tfHakim;
+    @FXML private TextField tfCari;
+
+    @FXML private ComboBox<String> cbSearchBy, cbFilterJenis;
+    @FXML private Label lblTotal;
+
+    private final ObservableList<Putusan> dataPutusan = FXCollections.observableArrayList();
+    private FilteredList<Putusan> filteredData;
+
+
