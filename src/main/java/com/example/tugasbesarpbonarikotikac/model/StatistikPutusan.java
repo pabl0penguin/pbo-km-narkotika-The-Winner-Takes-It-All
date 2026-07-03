a058906ee1f6b129bf6261fbd9f3a2220d37139d
unknown <raihansinatriaoktav@gmail.com>
2026-07-03 19:28:03 +0700

    feat: tambah atribut statistik putusan

--- src/main/java/com/example/tugasbesarpbonarikotikac/model/StatistikPutusan.java
+++ src/main/java/com/example/tugasbesarpbonarikotikac/model/StatistikPutusan.java
@@ -1,4 +1,11 @@
 package com.example.tugasbesarpbonarikotikac.model;
 
 public class StatistikPutusan {
+
+    private int totalPutusan;
+    private double rataRataVonis;
+    private double rataRataDenda;
+    private String jenisNarkotikaTerbanyak;
+    private String[] distribusiPeran;
+
 }
