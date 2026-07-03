5688dc33f09cad301ddd9f541fedd1dec0784b09
unknown <raihansinatriaoktav@gmail.com>
2026-07-03 19:35:36 +0700

    feat: tambah distribusi peran terdakwa

--- src/main/java/com/example/tugasbesarpbonarikotikac/model/StatistikPutusan.java
+++ src/main/java/com/example/tugasbesarpbonarikotikac/model/StatistikPutusan.java
@@ -40,6 +40,30 @@ public class StatistikPutusan {
         if (totalPutusan > 0) {
             rataRataDenda = totalDenda / totalPutusan;
         }
+
+        HashMap<String, Integer> peranMap = new HashMap<>();
+
+        for(Putusan p : daftar){
+
+            peranMap.put(
+                    p.getPeranTerdakwa(),
+                    peranMap.getOrDefault(
+                            p.getPeranTerdakwa(), 0
+                    ) + 1
+            );
+        }
+
+        distribusiPeran = new String[peranMap.size()];
+
+        int i = 0;
+
+        for(String peran : peranMap.keySet()){
+
+            distribusiPeran[i] =
+                    peran + " : " + peranMap.get(peran);
+
+            i++;
+        }
     }
 
 }
